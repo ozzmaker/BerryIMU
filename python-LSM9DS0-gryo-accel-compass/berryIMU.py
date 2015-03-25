5674e0423ca6354d21aa3f7949870e4550503708
mwilliams03 <mark.mwilliams@gmail.com>
2015-03-25 03:04:00 +0000

    Python code added

---
+++ python-LSM9DS0-gryo-accel-compass/berryIMU.py
@@ -0,0 +1,215 @@
+#!/usr/bin/python
+#
+#	This program  reads the angles from the acceleromter, gyrscope
+#	and mangnetometeron a BerryIMU connected to a Raspberry Pi.
+#
+#	http://ozzmaker.com/
+#
+#    Copyright (C) 2014  Mark Williams
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Library General Public
+#    License as published by the Free Software Foundation; either
+#    version 2 of the License, or (at your option) any later version.
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    Library General Public License for more details.
+#    You should have received a copy of the GNU Library General Public
+#    License along with this library; if not, write to the Free
+#    Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+#    MA 02111-1307, USA
+
+
+import smbus
+import time
+import math
+from LSM9DS0 import *
+import datetime
+bus = smbus.SMBus(1)
+
+RAD_TO_DEG = 57.29578
+M_PI = 3.14159265358979323846
+G_GAIN = 0.070  # [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
+LP = 0.041    	# Loop period = 41ms.   This needs to match the time it takes each loop to run
+AA =  0.90      # Complementary filter constant
+
+
+def writeACC(register,value):
+        bus.write_byte_data(ACC_ADDRESS , register, value)
+        return -1
+
+def writeMAG(register,value):
+        bus.write_byte_data(MAG_ADDRESS, register, value)
+        return -1
+
+def writeGRY(register,value):
+        bus.write_byte_data(GYR_ADDRESS, register, value)
+        return -1
+
+
+
+def readACCx():
+        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_X_L_A)
+        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_X_H_A)
+	acc_combined = (acc_l | acc_h <<8)
+
+	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
+
+
+def readACCy():
+        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Y_L_A)
+        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Y_H_A)
+	acc_combined = (acc_l | acc_h <<8)
+
+	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
+
+
+def readACCz():
+        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Z_L_A)
+        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Z_H_A)
+	acc_combined = (acc_l | acc_h <<8)
+
+	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
+
+
+def readMAGx():
+        mag_l = bus.read_byte_data(MAG_ADDRESS, OUT_X_L_M)
+        mag_h = bus.read_byte_data(MAG_ADDRESS, OUT_X_H_M)
+        mag_combined = (mag_l | mag_h <<8)
+
+        return mag_combined  if mag_combined < 32768 else mag_combined - 65536
+
+
+def readMAGy():
+        mag_l = bus.read_byte_data(MAG_ADDRESS, OUT_Y_L_M)
+        mag_h = bus.read_byte_data(MAG_ADDRESS, OUT_Y_H_M)
+        mag_combined = (mag_l | mag_h <<8)
+
+        return mag_combined  if mag_combined < 32768 else mag_combined - 65536
+
+
+def readMAGz():
+        mag_l = bus.read_byte_data(MAG_ADDRESS, OUT_Z_L_M)
+        mag_h = bus.read_byte_data(MAG_ADDRESS, OUT_Z_H_M)
+        mag_combined = (mag_l | mag_h <<8)
+
+        return mag_combined  if mag_combined < 32768 else mag_combined - 65536
+
+
+
+def readGYRx():
+        gyr_l = bus.read_byte_data(GYR_ADDRESS, OUT_X_L_G)
+        gyr_h = bus.read_byte_data(GYR_ADDRESS, OUT_X_H_G)
+        gyr_combined = (gyr_l | gyr_h <<8)
+
+        return gyr_combined  if gyr_combined < 32768 else gyr_combined - 65536
+  
+
+def readGYRy():
+        gyr_l = bus.read_byte_data(GYR_ADDRESS, OUT_Y_L_G)
+        gyr_h = bus.read_byte_data(GYR_ADDRESS, OUT_Y_H_G)
+        gyr_combined = (gyr_l | gyr_h <<8)
+
+        return gyr_combined  if gyr_combined < 32768 else gyr_combined - 65536
+
+def readGYRz():
+        gyr_l = bus.read_byte_data(GYR_ADDRESS, OUT_Z_L_G)
+        gyr_h = bus.read_byte_data(GYR_ADDRESS, OUT_Z_H_G)
+        gyr_combined = (gyr_l | gyr_h <<8)
+
+        return gyr_combined  if gyr_combined < 32768 else gyr_combined - 65536
+
+
+
+	
+#initialise the accelerometer
+writeACC(CTRL_REG1_XM, 0b01100111) #z,y,x axis enabled, continuos update,  100Hz data rate
+writeACC(CTRL_REG2_XM, 0b00100000) #+/- 16G full scale
+
+#initialise the magnetometer
+writeMAG(CTRL_REG5_XM, 0b11110000) #Temp enable, M data rate = 50Hz
+writeMAG(CTRL_REG6_XM, 0b01100000) #+/-12gauss
+writeMAG(CTRL_REG7_XM, 0b00000000) #Continuous-conversion mode
+
+#initialise the gyroscope
+writeGRY(CTRL_REG1_G, 0b00001111) #Normal power mode, all axes enabled
+writeGRY(CTRL_REG4_G, 0b00110000) #Continuos update, 2000 dps full scale
+
+gyroXangle = 0.0
+gyroYangle = 0.0
+gyroZangle = 0.0
+CFangleX = 0.0
+CFangleY = 0.0
+
+
+
+
+while True:
+	a = datetime.datetime.now()
+	
+	AccXangle =  (math.atan2(readACCy(),readACCz())+M_PI)*RAD_TO_DEG;
+	AccYangle =  (math.atan2(readACCz(),readACCx())+M_PI)*RAD_TO_DEG;
+
+
+	#Convert Gyro raw to degrees per second
+	rate_gyr_x =  readGYRx() * G_GAIN
+	rate_gyr_y =  readGYRy()  * G_GAIN
+	rate_gyr_z =  readGYRz()  * G_GAIN
+
+
+	#Calculate the angles from the gyro. LP = loop period 
+	gyroXangle+=rate_gyr_x*LP;
+	gyroYangle+=rate_gyr_y*LP;
+	gyroZangle+=rate_gyr_z*LP;
+
+
+
+
+
+        #Change the rotation value of the accelerometer to -/+ 180 and move the Y axis '0' point to up.
+        #Two different pieces of code are used depending on how your IMU is mounted.
+        #If IMU is upside down
+	#
+        #if AccXangle >180:
+        #        AccXangle -= 360.0
+        #AccYangle-=90
+        #if (AccYangle >180):
+        #        AccYangle -= 360.0
+	
+
+        #//If IMU is up the correct way, use these lines
+        AccXangle -= 180.0
+	if AccYangle > 90:
+	        AccYangle -= 270.0
+	else:
+		AccYangle += 90.0
+
+
+        #Complementary filter used to combine the accelerometer and gyro values.
+        CFangleX=AA*(CFangleX+rate_gyr_x*LP) +(1 - AA) * AccXangle;
+        CFangleY=AA*(CFangleY+rate_gyr_y*LP) +(1 - AA) * AccYangle;
+
+
+
+	heading = 180 * math.atan2(readMAGy(),readMAGx())/M_PI
+
+	if heading < 0:
+	 	heading += 360;
+
+
+ 	print ("\033[1;34;40mACCX Angle %5.2f ACCY Angle %5.2f\033[1;31;40m\tGRYX Angle %5.2f  GYRY Angle %5.2f  GYRZ Angle %5.2f \033[1;35;40m    \tCFangleX Angle %5.2f \033[1;36;40m  CFangleY Angle %5.2f \33[1;32;40m  HEADING  %5.2f\033[0m  " % (AccXangle, AccYangle,gyroXangle,gyroYangle,gyroZangle,CFangleX,CFangleY,heading))
+
+
+
+
+
+
+        time.sleep(0.03)
+	b = datetime.datetime.now()
+	c = b - a
+
+	print "Loop Time |",  c.microseconds/1000,"|",
+
+
+
+
