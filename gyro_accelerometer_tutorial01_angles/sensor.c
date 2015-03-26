c3bc6568fa9f532fd5649748d0ca82881cc18b2e
mwilliams03 <mark.mwilliams@gmail.com>
2015-03-26 21:59:41 +1100

    Update sensor.c

--- gyro_accelerometer_tutorial01_angles/sensor.c
+++ gyro_accelerometer_tutorial01_angles/sensor.c
@@ -70,9 +70,7 @@ void readMAG(int  *m)
 void readGYR(int *g)
 {
 	uint8_t block[6];
-
         selectDevice(file,GYR_ADDRESS);
-
 	readBlock(0x80 | OUT_X_L_G, sizeof(block), block);
 
         *g = (int16_t)(block[0] | block[1] << 8);
