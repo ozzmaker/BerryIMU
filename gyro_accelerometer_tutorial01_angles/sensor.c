d75fb3b8e3a503a6c2419e16d42273bfae13dd61
mwilliams03 <mark.mwilliams@gmail.com>
2015-03-26 21:57:54 +1100

    Update sensor.c
    add         selectDevice(file,MAG_ADDRESS);

--- gyro_accelerometer_tutorial01_angles/sensor.c
+++ gyro_accelerometer_tutorial01_angles/sensor.c
@@ -58,7 +58,7 @@ void readACC(int  *a)
 void readMAG(int  *m)
 {
         uint8_t block[6];
-
+        selectDevice(file,MAG_ADDRESS);
         readBlock(0x80 | OUT_X_L_M, sizeof(block), block);
 
         *m = (int16_t)(block[0] | block[1] << 8);
