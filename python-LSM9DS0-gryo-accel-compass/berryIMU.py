e95cb777e068287b9b164403ab589286a3413546
mwilliams03 <mark.mwilliams@gmail.com>
2015-06-07 18:25:13 +0000

    Added tilt compensation

--- python-LSM9DS0-gryo-accel-compass/berryIMU.py
+++ python-LSM9DS0-gryo-accel-compass/berryIMU.py
@@ -5,7 +5,7 @@
 #
 #	http://ozzmaker.com/
 #
-#    Copyright (C) 2014  Mark Williams
+#    Copyright (C) 2015  Mark Williams
 #    This library is free software; you can redistribute it and/or
 #    modify it under the terms of the GNU Library General Public
 #    License as published by the Free Software Foundation; either
@@ -31,7 +31,7 @@ RAD_TO_DEG = 57.29578
 M_PI = 3.14159265358979323846
 G_GAIN = 0.070  # [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
 LP = 0.041    	# Loop period = 41ms.   This needs to match the time it takes each loop to run
-AA =  0.90      # Complementary filter constant
+AA =  0.80      # Complementary filter constant
 
 
 def writeACC(register,value):
@@ -147,20 +147,33 @@ CFangleY = 0.0
 while True:
 	a = datetime.datetime.now()
 	
-	AccXangle =  (math.atan2(readACCy(),readACCz())+M_PI)*RAD_TO_DEG;
-	AccYangle =  (math.atan2(readACCz(),readACCx())+M_PI)*RAD_TO_DEG;
+	#Read our accelerometer,gyroscope and magnetometer  values
+	ACCx = readACCx()
+	ACCy = readACCy()
+	ACCz = readACCz()
+	GYRx = readGYRx()
+	GYRy = readGYRx()
+	GYRz = readGYRx()
+	MAGx = readMAGx()
+	MAGy = readMAGy()
+	MAGz = readMAGz()
+
+	##Convert Accelerometer values to degrees
+	AccXangle =  (math.atan2(ACCy,ACCz)+M_PI)*RAD_TO_DEG
+	AccYangle =  (math.atan2(ACCz,ACCx)+M_PI)*RAD_TO_DEG
+
 
 
 	#Convert Gyro raw to degrees per second
-	rate_gyr_x =  readGYRx() * G_GAIN
-	rate_gyr_y =  readGYRy()  * G_GAIN
-	rate_gyr_z =  readGYRz()  * G_GAIN
+	rate_gyr_x =  GYRx * G_GAIN
+	rate_gyr_y =  GYRy * G_GAIN
+	rate_gyr_z =  GYRz * G_GAIN
 
 
 	#Calculate the angles from the gyro. LP = loop period 
-	gyroXangle+=rate_gyr_x*LP;
-	gyroYangle+=rate_gyr_y*LP;
-	gyroZangle+=rate_gyr_z*LP;
+	gyroXangle+=rate_gyr_x*LP
+	gyroYangle+=rate_gyr_y*LP
+	gyroZangle+=rate_gyr_z*LP
 
 
 
@@ -177,7 +190,7 @@ while True:
         #        AccYangle -= 360.0
 	
 
-        #//If IMU is up the correct way, use these lines
+        #If IMU is up the correct way, use these lines
         AccXangle -= 180.0
 	if AccYangle > 90:
 	        AccYangle -= 270.0
@@ -186,22 +199,38 @@ while True:
 
 
         #Complementary filter used to combine the accelerometer and gyro values.
-        CFangleX=AA*(CFangleX+rate_gyr_x*LP) +(1 - AA) * AccXangle;
-        CFangleY=AA*(CFangleY+rate_gyr_y*LP) +(1 - AA) * AccYangle;
+        CFangleX=AA*(CFangleX+rate_gyr_x*LP) +(1 - AA) * AccXangle
+        CFangleY=AA*(CFangleY+rate_gyr_y*LP) +(1 - AA) * AccYangle
 
 
 
-	heading = 180 * math.atan2(readMAGy(),readMAGx())/M_PI
+	#Calculate heading
+	heading = 180 * math.atan2(MAGy,MAGx)/M_PI
 
 	if heading < 0:
-	 	heading += 360;
+	 	heading += 360
 
 
- 	print ("\033[1;34;40mACCX Angle %5.2f ACCY Angle %5.2f\033[1;31;40m\tGRYX Angle %5.2f  GYRY Angle %5.2f  GYRZ Angle %5.2f \033[1;35;40m    \tCFangleX Angle %5.2f \033[1;36;40m  CFangleY Angle %5.2f \33[1;32;40m  HEADING  %5.2f\033[0m  " % (AccXangle, AccYangle,gyroXangle,gyroYangle,gyroZangle,CFangleX,CFangleY,heading))
+	#Normalize accelerometer raw values.
+        accXnorm = ACCx/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+	accYnorm = ACCy/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
 
 
+	#Calculate pitch and roll
+	pitch = math.asin(accXnorm)
+	roll = -math.asin(accYnorm/math.cos(pitch))
 
+	#Calculate the new tilt compensated values
+	magXcomp = MAGx*math.cos(pitch)+MAGz*math.sin(pitch)
+	magYcomp = MAGx*math.sin(roll)*math.sin(pitch)+MAGy*math.cos(roll)-MAGz*math.sin(roll)*math.cos(pitch)
 
+	#Calculate tiles compensated heading
+        tiltCompensatedHeading = 180 * math.atan2(magYcomp,magXcomp)/M_PI
+
+        if tiltCompensatedHeading < 0:
+                tiltCompensatedHeading += 360
+
+ 	print ("\033[1;34;40mACCX Angle %5.2f ACCY Angle %5.2f\033[1;31;40m\tGRYX Angle %5.2f  GYRY Angle %5.2f  GYRZ Angle %5.2f \033[1;35;40m    \tCFangleX Angle %5.2f \033[1;36;40m  CFangleY Angle %5.2f \33[1;32;40m  HEADING  %5.2f \33[1;37;40m tiltCompensatedHeading %5.2f\033[0m  " % (AccXangle, AccYangle,gyroXangle,gyroYangle,gyroZangle,CFangleX,CFangleY,heading,tiltCompensatedHeading))
 
 
         time.sleep(0.03)
