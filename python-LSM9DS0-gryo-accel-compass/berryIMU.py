a3d78332d582453a6dc1f5fa121f814a57fc21bc
mwilliams03 <mark.mwilliams@gmail.com>
2016-03-21 05:23:00 +0000

    Add Kalman filter to Python code

--- python-LSM9DS0-gryo-accel-compass/berryIMU.py
+++ python-LSM9DS0-gryo-accel-compass/berryIMU.py
@@ -30,9 +30,104 @@ bus = smbus.SMBus(1)
 RAD_TO_DEG = 57.29578
 M_PI = 3.14159265358979323846
 G_GAIN = 0.070  # [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
-LP = 0.041    	# Loop period = 41ms.   This needs to match the time it takes each loop to run
 AA =  0.40      # Complementary filter constant
 
+#Kalman filter variables
+Q_angle = 0.02
+Q_gyro = 0.0015
+R_angle = 0.005
+y_bias = 0.0
+x_bias = 0.0
+XP_00 = 0.0
+XP_01 = 0.0
+XP_10 = 0.0
+XP_11 = 0.0
+YP_00 = 0.0
+YP_01 = 0.0
+YP_10 = 0.0
+YP_11 = 0.0
+KFangleX = 0.0
+KFangleY = 0.0
+
+
+
+def kalmanFilterY ( accAngle, gyroRate, DT):
+	y=0.0
+	S=0.0
+
+	global KFangleY
+	global Q_angle
+	global Q_gyro
+	global y_bias
+	global XP_00
+	global XP_01
+	global XP_10
+	global XP_11
+	global YP_00
+	global YP_01
+	global YP_10
+	global YP_11
+
+	KFangleY = KFangleY + DT * (gyroRate - y_bias)
+
+	YP_00 = YP_00 + ( - DT * (YP_10 + YP_01) + Q_angle * DT )
+	YP_01 = YP_01 + ( - DT * YP_11 )
+	YP_10 = YP_10 + ( - DT * YP_11 )
+	YP_11 = YP_11 + ( + Q_gyro * DT )
+
+	y = accAngle - KFangleY
+	S = YP_00 + R_angle
+	K_0 = YP_00 / S
+	K_1 = YP_10 / S
+	
+	KFangleY = KFangleY + ( K_0 * y )
+	y_bias = y_bias + ( K_1 * y )
+	
+	YP_00 = YP_00 - ( K_0 * YP_00 )
+	YP_01 = YP_01 - ( K_0 * YP_01 )
+	YP_10 = YP_10 - ( K_1 * YP_00 )
+	YP_11 = YP_11 - ( K_1 * YP_01 )
+	
+	return KFangleY
+
+def kalmanFilterX ( accAngle, gyroRate, DT):
+	x=0.0
+	S=0.0
+
+	global KFangleX
+	global Q_angle
+	global Q_gyro
+	global x_bias
+	global XP_00
+	global XP_01
+	global XP_10
+	global XP_11
+	global YP_00
+	global YP_01
+	global YP_10
+	global YP_11
+
+	KFangleX = KFangleX + DT * (gyroRate - x_bias)
+
+	YP_00 = YP_00 + ( - DT * (YP_10 + YP_01) + Q_angle * DT )
+	YP_01 = YP_01 + ( - DT * YP_11 )
+	YP_10 = YP_10 + ( - DT * YP_11 )
+	YP_11 = YP_11 + ( + Q_gyro * DT )
+
+	x = accAngle - KFangleX
+	S = YP_00 + R_angle
+	K_0 = YP_00 / S
+	K_1 = YP_10 / S
+	
+	KFangleX = KFangleX + ( K_0 * x )
+	x_bias = x_bias + ( K_1 * x )
+	
+	YP_00 = YP_00 - ( K_0 * YP_00 )
+	YP_01 = YP_01 - ( K_0 * YP_01 )
+	YP_10 = YP_10 - ( K_1 * YP_00 )
+	YP_11 = YP_11 - ( K_1 * YP_01 )
+	
+	return KFangleX
 
 def writeACC(register,value):
         bus.write_byte_data(ACC_ADDRESS , register, value)
@@ -140,14 +235,15 @@ gyroYangle = 0.0
 gyroZangle = 0.0
 CFangleX = 0.0
 CFangleY = 0.0
+kalmanX = 0.0
+kalmanY = 0.0
 
-
-
+a = datetime.datetime.now()
 
 while True:
-	a = datetime.datetime.now()
 	
-	#Read our accelerometer,gyroscope and magnetometer  values
+	
+	#Read the accelerometer,gyroscope and magnetometer values
 	ACCx = readACCx()
 	ACCy = readACCy()
 	ACCz = readACCz()
@@ -157,74 +253,101 @@ while True:
 	MAGx = readMAGx()
 	MAGy = readMAGy()
 	MAGz = readMAGz()
-
-	##Convert Accelerometer values to degrees
-	AccXangle =  (math.atan2(ACCy,ACCz)+M_PI)*RAD_TO_DEG
-	AccYangle =  (math.atan2(ACCz,ACCx)+M_PI)*RAD_TO_DEG
-
-
-
+	
+	##Calculate loop Period(LP). How long between Gyro Reads
+	b = datetime.datetime.now() - a
+	a = datetime.datetime.now()
+	LP = b.microseconds/(1000000*1.0)
+	print "Loop Time | %5.2f|" % ( LP ),
+	
+	
 	#Convert Gyro raw to degrees per second
 	rate_gyr_x =  GYRx * G_GAIN
 	rate_gyr_y =  GYRy * G_GAIN
 	rate_gyr_z =  GYRz * G_GAIN
 
 
-	#Calculate the angles from the gyro. LP = loop period 
+	#Calculate the angles from the gyro. 
 	gyroXangle+=rate_gyr_x*LP
 	gyroYangle+=rate_gyr_y*LP
 	gyroZangle+=rate_gyr_z*LP
 
 
-
-
-	###################Correct rotation value########################
-        #Change the rotation value of the accelerometer to -/+ 180 and move the Y axis '0' point to up.
-        #Two different pieces of code are used depending on how your IMU is mounted.
-        #If IMU is upside down Skull logo is facing up
-	#
-        #if AccXangle >180:
-        #        AccXangle -= 360.0
-        #AccYangle-=90
-        #if (AccYangle >180):
-        #        AccYangle -= 360.0
+	##Convert Accelerometer values to degrees
+	AccXangle =  (math.atan2(ACCy,ACCz)+M_PI)*RAD_TO_DEG
+	AccYangle =  (math.atan2(ACCz,ACCx)+M_PI)*RAD_TO_DEG
 	
-
-        #If IMU is up the correct way, Skull logo is facing down. use these lines
-        AccXangle -= 180.0
+	
+	####################################################################
+	######################Correct rotation value########################
+	####################################################################
+    #Change the rotation value of the accelerometer to -/+ 180 and
+    #move the Y axis '0' point to up.
+    #
+    #Two different pieces of code are used depending on how your IMU is mounted.
+	#If IMU is up the correct way, Skull logo is facing down, Use these lines
+	AccXangle -= 180.0
 	if AccYangle > 90:
-	        AccYangle -= 270.0
+		AccYangle -= 270.0
 	else:
 		AccYangle += 90.0
+	#
+	#
+	#
+	#
+	#If IMU is upside down E.g Skull logo is facing up;
+	#if AccXangle >180:
+    #        AccXangle -= 360.0
+	#AccYangle-=90
+	#if (AccYangle >180):
+    #        AccYangle -= 360.0
 	############################ END ##################################
 
 
-        #Complementary filter used to combine the accelerometer and gyro values.
-        CFangleX=AA*(CFangleX+rate_gyr_x*LP) +(1 - AA) * AccXangle
-        CFangleY=AA*(CFangleY+rate_gyr_y*LP) +(1 - AA) * AccYangle
-
-
-
+	#Complementary filter used to combine the accelerometer and gyro values.
+	CFangleX=AA*(CFangleX+rate_gyr_x*LP) +(1 - AA) * AccXangle
+	CFangleY=AA*(CFangleY+rate_gyr_y*LP) +(1 - AA) * AccYangle
+	
+	#Kalman filter used to combine the accelerometer and gyro values.
+	kalmanY = kalmanFilterY(AccYangle, rate_gyr_y,LP)
+	kalmanX = kalmanFilterX(AccXangle, rate_gyr_x,LP)
+	
+	
+	####################################################################
+	############################MAG direction ##########################
+	####################################################################
+	#If IMU is upside down, then use this line.  It isnt needed if the
+	# IMU is the correct way up
+	#MAGy = -MAGy
+	#
+	############################ END ##################################
+	
+	
 	#Calculate heading
 	heading = 180 * math.atan2(MAGy,MAGx)/M_PI
 
+	#Only have our heading between 0 and 360
 	if heading < 0:
 	 	heading += 360
 
 
 	#Normalize accelerometer raw values.
-        accXnorm = ACCx/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+	accXnorm = ACCx/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
 	accYnorm = ACCy/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
-
+	
+	####################################################################
 	###################Calculate pitch and roll#########################
+	####################################################################
 	#Us these two lines when the IMU is up the right way. Skull logo is facing down
 	pitch = math.asin(accXnorm)
 	roll = -math.asin(accYnorm/math.cos(pitch))
-
-	#Us these two lines when the IMU is upside down. Skull logo is facing up
-	#pitch = -math.asin(accXnorm)
+	#
+	#Us these four lines when the IMU is upside down. Skull logo is facing up
+	#accXnorm = -accXnorm				#flip Xnorm as the IMU is upside down
+	#accYnorm = -accYnorm				#flip Ynorm as the IMU is upside down
+	#pitch = math.asin(accXnorm)
 	#roll = math.asin(accYnorm/math.cos(pitch))
-
+	#
 	############################ END ##################################
 
 	#Calculate the new tilt compensated values
@@ -238,14 +361,9 @@ while True:
                 tiltCompensatedHeading += 360
 
  	print ("\033[1;34;40mACCX Angle %5.2f ACCY Angle %5.2f\033[1;31;40m\tGRYX Angle %5.2f  GYRY Angle %5.2f  GYRZ Angle %5.2f \033[1;35;40m    \tCFangleX Angle %5.2f \033[1;36;40m  CFangleY Angle %5.2f \33[1;32;40m  HEADING  %5.2f \33[1;37;40m tiltCompensatedHeading %5.2f\033[0m  " % (AccXangle, AccYangle,gyroXangle,gyroYangle,gyroZangle,CFangleX,CFangleY,heading,tiltCompensatedHeading))
+	#print ("\033[1;31;40mCFangleX %5.2f \033[1;35;40m  CFangleY %5.2f \33[1;32;40m   \033[1;31;40m kalmanX %5.2f  \033[1;35;40m kalmanY %5.2f  " % (CFangleX,CFangleY,kalmanX,kalmanY))
 
-
-        time.sleep(0.03)
-	b = datetime.datetime.now()
-	c = b - a
-
-	print "Loop Time |",  c.microseconds/1000,"|",
-
-
+	#slow program down a bit
+	time.sleep(0.03)
 
 
