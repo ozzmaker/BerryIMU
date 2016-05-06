88aefe99af20983989eebad3e8b347fc4015728d
mwilliams03 <mark.mwilliams@gmail.com>
2016-05-06 08:40:44 +0000

    added simple code

--- python-BerryIMU-gryo-accel-compass/berryIMU-simple.py
+++ python-BerryIMU-gryo-accel-compass/berryIMU-simple.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python
+#	This is the base code needed to get usable angles from a BerryIMU 
+#	using a Complementary filter. The readings can be improved by 
+#	adding more filters, E.g Kalman, Low pass, median filter, etc..
 #
-#	This program  reads the angles from the acceleromter, gyrscope
-#	and mangnetometeron a BerryIMU connected to a Raspberry Pi.
-#
-#	This program includes a number of calculations to improve the 
-#	values returned from BerryIMU. If this is new to you, it 
-#	may be worthwhile first to look at berryIMU-simple.py, which 
-#	has a much more simplified version of code which would be easier
-#	to read.   
-#
-#
+#	For this code to work correctly, BerryIMU must be facing the
+#	correct way up. This is when the Skull Logo on the PCB is facing down.
 #	http://ozzmaker.com/
 #
 #    Copyright (C) 2016  Mark Williams
@@ -34,107 +29,13 @@ from LSM9DS0 import *
 import datetime
 bus = smbus.SMBus(1)
 
+
 RAD_TO_DEG = 57.29578
 M_PI = 3.14159265358979323846
 G_GAIN = 0.070  # [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
 AA =  0.40      # Complementary filter constant
 
-#Kalman filter variables
-Q_angle = 0.02
-Q_gyro = 0.0015
-R_angle = 0.005
-y_bias = 0.0
-x_bias = 0.0
-XP_00 = 0.0
-XP_01 = 0.0
-XP_10 = 0.0
-XP_11 = 0.0
-YP_00 = 0.0
-YP_01 = 0.0
-YP_10 = 0.0
-YP_11 = 0.0
-KFangleX = 0.0
-KFangleY = 0.0
 
-
-
-def kalmanFilterY ( accAngle, gyroRate, DT):
-	y=0.0
-	S=0.0
-
-	global KFangleY
-	global Q_angle
-	global Q_gyro
-	global y_bias
-	global XP_00
-	global XP_01
-	global XP_10
-	global XP_11
-	global YP_00
-	global YP_01
-	global YP_10
-	global YP_11
-
-	KFangleY = KFangleY + DT * (gyroRate - y_bias)
-
-	YP_00 = YP_00 + ( - DT * (YP_10 + YP_01) + Q_angle * DT )
-	YP_01 = YP_01 + ( - DT * YP_11 )
-	YP_10 = YP_10 + ( - DT * YP_11 )
-	YP_11 = YP_11 + ( + Q_gyro * DT )
-
-	y = accAngle - KFangleY
-	S = YP_00 + R_angle
-	K_0 = YP_00 / S
-	K_1 = YP_10 / S
-	
-	KFangleY = KFangleY + ( K_0 * y )
-	y_bias = y_bias + ( K_1 * y )
-	
-	YP_00 = YP_00 - ( K_0 * YP_00 )
-	YP_01 = YP_01 - ( K_0 * YP_01 )
-	YP_10 = YP_10 - ( K_1 * YP_00 )
-	YP_11 = YP_11 - ( K_1 * YP_01 )
-	
-	return KFangleY
-
-def kalmanFilterX ( accAngle, gyroRate, DT):
-	x=0.0
-	S=0.0
-
-	global KFangleX
-	global Q_angle
-	global Q_gyro
-	global x_bias
-	global XP_00
-	global XP_01
-	global XP_10
-	global XP_11
-	global YP_00
-	global YP_01
-	global YP_10
-	global YP_11
-
-	KFangleX = KFangleX + DT * (gyroRate - x_bias)
-
-	YP_00 = YP_00 + ( - DT * (YP_10 + YP_01) + Q_angle * DT )
-	YP_01 = YP_01 + ( - DT * YP_11 )
-	YP_10 = YP_10 + ( - DT * YP_11 )
-	YP_11 = YP_11 + ( + Q_gyro * DT )
-
-	x = accAngle - KFangleX
-	S = YP_00 + R_angle
-	K_0 = YP_00 / S
-	K_1 = YP_10 / S
-	
-	KFangleX = KFangleX + ( K_0 * x )
-	x_bias = x_bias + ( K_1 * x )
-	
-	YP_00 = YP_00 - ( K_0 * YP_00 )
-	YP_01 = YP_01 - ( K_0 * YP_01 )
-	YP_10 = YP_10 - ( K_1 * YP_00 )
-	YP_11 = YP_11 - ( K_1 * YP_01 )
-	
-	return KFangleX
 
 def writeACC(register,value):
         bus.write_byte_data(ACC_ADDRESS , register, value)
@@ -242,8 +143,7 @@ gyroYangle = 0.0
 gyroZangle = 0.0
 CFangleX = 0.0
 CFangleY = 0.0
-kalmanX = 0.0
-kalmanY = 0.0
+
 
 a = datetime.datetime.now()
 
@@ -280,54 +180,24 @@ while True:
 	gyroZangle+=rate_gyr_z*LP
 
 
-	##Convert Accelerometer values to degrees
+	#Convert Accelerometer values to degrees
 	AccXangle =  (math.atan2(ACCy,ACCz)+M_PI)*RAD_TO_DEG
 	AccYangle =  (math.atan2(ACCz,ACCx)+M_PI)*RAD_TO_DEG
 	
 	
-	####################################################################
-	######################Correct rotation value########################
-	####################################################################
-	#Change the rotation value of the accelerometer to -/+ 180 and
-    	#move the Y axis '0' point to up.
-    	#
-    	#Two different pieces of code are used depending on how your IMU is mounted.
-	#If IMU is up the correct way, Skull logo is facing down, Use these lines
+	#convert the values to -180 and +180
 	AccXangle -= 180.0
 	if AccYangle > 90:
 		AccYangle -= 270.0
 	else:
 		AccYangle += 90.0
-	#
-	#
-	#
-	#
-	#If IMU is upside down E.g Skull logo is facing up;
-	#if AccXangle >180:
-    	#        AccXangle -= 360.0
-	#AccYangle-=90
-	#if (AccYangle >180):
-    	#        AccYangle -= 360.0
-	############################ END ##################################
-
+	
+	
 
 	#Complementary filter used to combine the accelerometer and gyro values.
 	CFangleX=AA*(CFangleX+rate_gyr_x*LP) +(1 - AA) * AccXangle
 	CFangleY=AA*(CFangleY+rate_gyr_y*LP) +(1 - AA) * AccYangle
 	
-	#Kalman filter used to combine the accelerometer and gyro values.
-	kalmanY = kalmanFilterY(AccYangle, rate_gyr_y,LP)
-	kalmanX = kalmanFilterX(AccXangle, rate_gyr_x,LP)
-	
-	
-	####################################################################
-	############################MAG direction ##########################
-	####################################################################
-	#If IMU is upside down, then use this line.  It isnt needed if the
-	# IMU is the correct way up
-	#MAGy = -MAGy
-	#
-	############################ END ##################################
 	
 	
 	#Calculate heading
@@ -342,20 +212,11 @@ while True:
 	accXnorm = ACCx/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
 	accYnorm = ACCy/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
 	
-	####################################################################
-	###################Calculate pitch and roll#########################
-	####################################################################
-	#Us these two lines when the IMU is up the right way. Skull logo is facing down
+	
+	#Calculate pitch and roll
 	pitch = math.asin(accXnorm)
 	roll = -math.asin(accYnorm/math.cos(pitch))
-	#
-	#Us these four lines when the IMU is upside down. Skull logo is facing up
-	#accXnorm = -accXnorm				#flip Xnorm as the IMU is upside down
-	#accYnorm = -accYnorm				#flip Ynorm as the IMU is upside down
-	#pitch = math.asin(accXnorm)
-	#roll = math.asin(accYnorm/math.cos(pitch))
-	#
-	############################ END ##################################
+
 
 	#Calculate the new tilt compensated values
 	magXcomp = MAGx*math.cos(pitch)+MAGz*math.sin(pitch)
@@ -379,10 +240,10 @@ while True:
 		print ("\033[1;35;40m   \tCFangleX Angle %5.2f \033[1;36;40m  CFangleY Angle %5.2f \33[1;32;40m" % (CFangleX,CFangleY)),
 		
  	if 1:			#Change to '0' to stop  showing the heading
-		print ("HEADING  %5.2f \33[1;37;40m tiltCompensatedHeading %5.2f" % (heading,tiltCompensatedHeading)),
+		print ("HEADING  %5.2f \33[1;37;40m tiltCompensatedHeading %5.2f" % (heading,tiltCompensatedHeading))
 		
-	if 1:			#Change to '0' to stop  showing the angles from the Kalman filter
-		print ("\033[1;31;40m kalmanX %5.2f  \033[1;35;40m kalmanY %5.2f  " % (kalmanX,kalmanY))
+
+
 
 	
 	#slow program down a bit, makes the output more readable
