224e2204cddfef1c5187647572322139b847609f
mwilliams03 <mark.mwilliams03@gmail.com>
2016-06-02 07:07:45 +0000

    Added G measurement

---
+++ python-BerryIMU-measure-G/berryIMU-measure-G.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+#	This is the base code needed to get usable angles from a BerryIMU 
+#	using a Complementary filter. The readings can be improved by 
+#	adding more filters, E.g Kalman, Low pass, median filter, etc..
+#
+#	For this code to work correctly, BerryIMU must be facing the
+#	correct way up. This is when the Skull Logo on the PCB is facing down.
+#	http://ozzmaker.com/
+#
+#    Copyright (C) 2016  Mark Williams
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Library General Public
+#    License as published by the Free Software Foundation; either
+#    version 2 of the License, or (at your option) any later version.
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    Library General Public License for more details.
+#    You should have received a copy of the GNU Library General Public
+#    License along with this library; if not, write to the Free
+#    Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+#    MA 02111-1307, USA
+
+
+import smbus
+import time
+import math
+from LSM9DS0 import *
+import datetime
+bus = smbus.SMBus(1)
+
+
+
+
+def writeACC(register,value):
+        bus.write_byte_data(ACC_ADDRESS , register, value)
+        return -1
+
+
+
+
+def readACCx():
+        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_X_L_A)
+        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_X_H_A)
+	acc_combined = (acc_l | acc_h <<8)
+
+	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
+
+
+def readACCy():
+        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Y_L_A)
+        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Y_H_A)
+	acc_combined = (acc_l | acc_h <<8)
+
+	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
+
+
+def readACCz():
+        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Z_L_A)
+        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Z_H_A)
+	acc_combined = (acc_l | acc_h <<8)
+
+	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
+
+
+
+
+	
+#initialise the accelerometer
+writeACC(CTRL_REG1_XM, 0b01100111) #z,y,x axis enabled, continuos update,  100Hz data rate
+writeACC(CTRL_REG2_XM, 0b00010000) #+/- 8G full scale
+
+
+
+while True:
+	
+	
+	#Read the accelerometer,gyroscope and magnetometer values
+	ACCx = readACCx()
+	ACCy = readACCy()
+	ACCz = readACCz()
+
+	print("##### X = %f G  #####" % ((ACCx * 0.224)/1000)),
+	print(" Y =   %fG  #####" % ((ACCy * 0.224)/1000)),
+	print(" Z =  %fG  #####" % ((ACCz * 0.224)/1000))
+
+	
+	
+	#slow program down a bit, makes the output more readable
+	time.sleep(0.03)
+
+
