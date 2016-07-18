00e36da35701f0adce559956b856568a21c4b39e
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2016-07-18 10:32:15 +0800

    Source code formatting
    - make proper naming of variables and functions
    - set correct function input parameter types
    - remove unused code
    - add option to choose BerryIMU mounting position
    - print values in order
    - text formatting for better view

--- gyro_accelerometer_tutorial01_angles/gyro_accelerometer_tutorial01.c
+++ gyro_accelerometer_tutorial01_angles/gyro_accelerometer_tutorial01.c
@@ -20,8 +20,6 @@
     MA 02111-1307, USA
 */
 
-
-
 #include <unistd.h>
 #include <math.h>
 #include <signal.h>
@@ -32,133 +30,109 @@
 #include <time.h>
 #include "sensor.c"
 
+#define DT 0.02       // [s/loop] loop period. 20ms
+#define AA 0.97       // complementary filter constant
 
-#define DT 0.02         // [s/loop] loop period. 20ms
-#define AA 0.97         // complementary filter constant
+#define A_GAIN 0.0573 // [deg/LSB]
+#define G_GAIN 0.070  // [deg/s/LSB]
 
-#define A_GAIN 0.0573      // [deg/LSB]
-#define G_GAIN 0.070     // [deg/s/LSB]
-#define RAD_TO_DEG 57.29578
-#define M_PI 3.14159265358979323846
+#ifndef PI
 
+#define PI 3.14159265
 
+#endif // !PI
 
+#define RAD_TO_DEG 180 / PI
 
-void  INThandler(int sig)
+void INThandler(int sig)
 {
-        signal(sig, SIG_IGN);
-        exit(0);
+	signal(sig, SIG_IGN);
+	exit(0);
 }
 
-int mymillis()
+int getMillisecondsTime()
 {
-	struct timeval tv;
-	gettimeofday(&tv, NULL);
-	return (tv.tv_sec) * 1000 + (tv.tv_usec)/1000;
+	struct timeval value;
+	gettimeofday(&value, NULL);
+
+	return (value.tv_sec) * 1000 + (value.tv_usec) / 1000;
 }
 
-int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
+int main()
 {
-    long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
-    result->tv_sec = diff / 1000000;
-    result->tv_usec = diff % 1000000;
-    return (diff<0);
-}
-
-int main(int argc, char *argv[])
-{
-
-	float rate_gyr_y = 0.0;   // [deg/s]
-	float rate_gyr_x = 0.0;    // [deg/s]
-	float rate_gyr_z = 0.0;     // [deg/s]
-
-	int  accRaw[3];
-	int  magRaw[3];
-	int  gyrRaw[3];
-
-
-
-	float gyroXangle = 0.0;
-	float gyroYangle = 0.0;
-	float gyroZangle = 0.0;
-	float AccYangle = 0.0;
-	float AccXangle = 0.0;
-	float CFangleX = 0.0;
-	float CFangleY = 0.0;
-
-	int startInt  = mymillis();
-	struct  timeval tvBegin, tvEnd,tvDiff;
-
-
-        signal(SIGINT, INThandler);
+	signal(SIGINT, INThandler);
 
 	enableIMU();
 
-	gettimeofday(&tvBegin, NULL);
+	float gyroscore_angle_X = 0;
+	float gyroscore_angle_Y = 0;
+	float gyroscore_angle_Z = 0;
 
+	float real_angle_X = 0;
+	float real_angle_Y = 0;
 
-	while(1)
-	{
-	startInt = mymillis();
+	char IMU_upside_down = 0;
 
+	while( 1 ) {
+		int startInt = getMillisecondsTime();
+		
+		// read accelerometer and gyroscope data
 
-	//read ACC and GYR data
-	readACC(accRaw);
-	readGYR(gyrRaw);
+		int16_t accRaw[3];
+		int16_t gyrRaw[3];
 
-	//Convert Gyro raw to degrees per second
-	rate_gyr_x = (float) gyrRaw[0] * G_GAIN;
-	rate_gyr_y = (float) gyrRaw[1]  * G_GAIN;
-	rate_gyr_z = (float) gyrRaw[2]  * G_GAIN;
+		readAccelerometer(accRaw);
+		readGyroscope(gyrRaw);
+		
+		//Convert Gyro raw to degrees per second
+		float gyroscore_rate_X = gyrRaw[0] * G_GAIN;
+		float gyroscore_rate_Y = gyrRaw[1] * G_GAIN;
+		float gyroscore_rate_Z = gyrRaw[2] * G_GAIN;
+		
+		//Calculate the angles from the gyro
+		gyroscore_angle_X += gyroscore_rate_X * DT;
+		gyroscore_angle_Y += gyroscore_rate_Y * DT;
+		gyroscore_angle_Z += gyroscore_rate_Z * DT;
+		
+		//Convert Accelerometer values to degrees
+		float accelerometer_angle_X = (atan2f(accRaw[1], accRaw[2]) + PI) * RAD_TO_DEG;
+		float accelerometer_angle_Y = (atan2f(accRaw[2], accRaw[0]) + PI) * RAD_TO_DEG;
+		
+		// Change the rotation value of the accelerometer to -/+ 180 and move the Y axis '0' point to up
+		// Two different pieces of code are used depending on how your IMU is mounted
+		if( IMU_upside_down ) { // if IMU is upside down
+			if( accelerometer_angle_X > 180 )
+				accelerometer_angle_X -= 360;
+		
+		    accelerometer_angle_Y -= 90;
+		    
+			if( accelerometer_angle_Y > 180 )
+				accelerometer_angle_Y -= 360;
+		}
+		else { // if IMU is up the correct way
+			accelerometer_angle_X -= 180;
 
+			if (accelerometer_angle_Y > 90)
+				accelerometer_angle_Y -= 270;
+			else
+				accelerometer_angle_Y += 90;
+		}
+		
+		//Complementary filter used to combine the accelerometer and gyro values.
+		real_angle_X = AA * (real_angle_X + gyroscore_rate_X * DT) + (1 - AA) * accelerometer_angle_X;
+		real_angle_Y = AA * (real_angle_Y + gyroscore_rate_Y * DT) + (1 - AA) * accelerometer_angle_Y;
+		
+		printf( "Gyroscore [%7.3f %7.3f], Accelerometer [%7.3f %7.3f], Result [%7.3f %7.3f]\n",
+				gyroscore_angle_X, gyroscore_angle_Y, accelerometer_angle_X, accelerometer_angle_Y, real_angle_X, real_angle_Y);
 
-
-	//Calculate the angles from the gyro
-	gyroXangle+=rate_gyr_x*DT;
-	gyroYangle+=rate_gyr_y*DT;
-	gyroZangle+=rate_gyr_z*DT;
-
-
-
-
-	//Convert Accelerometer values to degrees
-	AccXangle = (float) (atan2(accRaw[1],accRaw[2])+M_PI)*RAD_TO_DEG;
-	AccYangle = (float) (atan2(accRaw[2],accRaw[0])+M_PI)*RAD_TO_DEG;
-
-        //Change the rotation value of the accelerometer to -/+ 180 and move the Y axis '0' point to up.
-        //Two different pieces of code are used depending on how your IMU is mounted.
-        //If IMU is upside down
-	/*
-        if (AccXangle >180)
-                AccXangle -= (float)360.0;
-
-        AccYangle-=90;
-        if (AccYangle >180)
-                AccYangle -= (float)360.0;
-	*/
-
-        //If IMU is up the correct way, use these lines
-        AccXangle -= (float)180.0;
-	if (AccYangle > 90)
-	        AccYangle -= (float)270;
-	else
-		AccYangle += (float)90;
-
-
-	//Complementary filter used to combine the accelerometer and gyro values.
-	CFangleX=AA*(CFangleX+rate_gyr_x*DT) +(1 - AA) * AccXangle;
-	CFangleY=AA*(CFangleY+rate_gyr_y*DT) +(1 - AA) * AccYangle;
-
-
-	printf ("   GyroX  %7.3f \t AccXangle \e[m %7.3f \t \033[22;31mCFangleX %7.3f\033[0m\t GyroY  %7.3f \t AccYangle %7.3f \t \033[22;36mCFangleY %7.3f\t\033[0m\n",gyroXangle,AccXangle,CFangleX,gyroYangle,AccYangle,CFangleY);
-
-	//Each loop should be at least 20ms.
-        while(mymillis() - startInt < (DT*1000))
-        {
-            usleep(100);
-        }
-
-	printf("Loop Time %d\t", mymillis()- startInt);
-    }
+		//printf(" GyroX  %7.3f \t AccXangle \e[m %7.3f \t \033[22;31mCFangleX %7.3f\033[0m\t GyroY  %7.3f \t AccYangle %7.3f \t \033[22;36mCFangleY %7.3f\t\033[0m\n",gyroXangle,AccXangle,CFangleX,gyroYangle,AccYangle,CFangleY);
+		
+		//Each loop should be at least 20ms.
+		while(getMillisecondsTime() - startInt < (DT*1000)) {
+		    usleep(100);
+		}
+		
+		printf("Loop time %d ms. ", getMillisecondsTime() - startInt);
+	}
 }
 
