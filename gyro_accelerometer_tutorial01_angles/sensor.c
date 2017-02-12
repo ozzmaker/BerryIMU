c37d43c6dc9b72416a7223f9902781ff55df0e87
mwilliams03 <mark.mwilliams@gmail.com>
2017-02-13 08:57:12 +1100

    Corrected typo in comment

--- gyro_accelerometer_tutorial01_angles/sensor.c
+++ gyro_accelerometer_tutorial01_angles/sensor.c
@@ -19,6 +19,7 @@
     Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
     MA 02111-1307, USA
 */
+
 #include <stdint.h>
 #include "linux/i2c-dev.h"
 #include "LSM9DS0.h"
@@ -135,9 +136,9 @@ void enableIMU()
         writeAccReg(CTRL_REG2_XM, 0b00100000); // +/- 16G full scale
 
         //Enable the magnetometer
-        writeMagReg( CTRL_REG5_XM, 0b11110000);   // Temp enable, M data rate = 50Hz
-        writeMagReg( CTRL_REG6_XM, 0b01100000);   // +/-12gauss
-        writeMagReg( CTRL_REG7_XM, 0b00000000);   // Continuous-conversion mode
+        writeMagReg( CTRL_REG5_XM, 0b11110000); // Temp enable, M data rate = 50Hz
+        writeMagReg( CTRL_REG6_XM, 0b01100000); // +/-12gauss
+        writeMagReg( CTRL_REG7_XM, 0b00000000); // Continuous-conversion mode
 
 	 // Enable Gyro
         writeGyrReg(CTRL_REG1_G, 0b00001111); // Normal power mode, all axes enabled
