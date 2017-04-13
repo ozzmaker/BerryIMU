93c10f51116efbca383dced8abf69815188b029c
mwilliams03 <mark.mwilliams@gmail.com>
2017-04-13 17:05:55 +1000

    updated print statements

--- python-BerryIMU-gryo-accel-compass/berryIMU.py
+++ python-BerryIMU-gryo-accel-compass/berryIMU.py
@@ -376,7 +376,10 @@ while True:
 		print ("HEADING  %5.2f \33[1;37;40m tiltCompensatedHeading %5.2f" % (heading,tiltCompensatedHeading)),
 		
 	if 1:			#Change to '0' to stop  showing the angles from the Kalman filter
-		print ("\033[1;31;40m kalmanX %5.2f  \033[1;35;40m kalmanY %5.2f  " % (kalmanX,kalmanY))
+		print ("\033[1;31;40m kalmanX %5.2f  \033[1;35;40m kalmanY %5.2f  " % (kalmanX,kalmanY)),
+	
+	#print a new line
+	print ""   
 
 	
 	#slow program down a bit, makes the output more readable
