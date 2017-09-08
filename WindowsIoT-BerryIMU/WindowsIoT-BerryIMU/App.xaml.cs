4fd816f55c2e364f5b5b02ad4ed3ba87e0e2316c
mwilliams03 <mark.mwilliams@gmail.com>
2017-09-08 17:42:42 +1000

    Update App.xaml.cs

--- WindowsIoT-BerryIMU/WindowsIoT-BerryIMU/App.xaml.cs
+++ WindowsIoT-BerryIMU/WindowsIoT-BerryIMU/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace BERRYIMU
+namespace BerryImu
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
