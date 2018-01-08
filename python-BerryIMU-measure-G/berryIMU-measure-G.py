c16088737d51802357b6287541bde4e820eec683
peterp-ozzmaker <peterp@ozzmaker.com>
2018-01-08 06:36:06 +0000

    added support for BerryIMUv2

--- python-BerryIMU-measure-G/berryIMU-measure-G.py
+++ python-BerryIMU-measure-G/berryIMU-measure-G.py
@@ -1,74 +1,36 @@
 #!/usr/bin/python
-#    Copyright (C) 2016  Mark Williams
-#    This library is free software; you can redistribute it and/or
-#    modify it under the terms of the GNU Library General Public
-#    License as published by the Free Software Foundation; either
-#    version 2 of the License, or (at your option) any later version.
-#    This library is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#    Library General Public License for more details.
-#    You should have received a copy of the GNU Library General Public
-#    License along with this library; if not, write to the Free
-#    Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-#    MA 02111-1307, USA
+#
+#	This program demonstrates how to convert the raw values from an accelerometer to Gs
+#
+#
+#   Both the BerryIMUv1 and BerryIMUv2 are supported
+#
+#   Feel free to do whatever you like with this code
+#	Distributed as-is; no warranty is given.
+#	
+#	http://ozzmaker.com/
 
 
-import smbus
-from LSM9DS0 import *
+
 import time
-bus = smbus.SMBus(1)
+import IMU
 
 
 
 
-def writeACC(register,value):
-        bus.write_byte_data(ACC_ADDRESS , register, value)
-        return -1
-
-
-
-
-def readACCx():
-        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_X_L_A)
-        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_X_H_A)
-	acc_combined = (acc_l | acc_h <<8)
-
-	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
-
-
-def readACCy():
-        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Y_L_A)
-        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Y_H_A)
-	acc_combined = (acc_l | acc_h <<8)
-
-	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
-
-
-def readACCz():
-        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Z_L_A)
-        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Z_H_A)
-	acc_combined = (acc_l | acc_h <<8)
-
-	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
-
-
-
+IMU.detectIMU()     #Detect if BerryIMUv1 or BerryIMUv2 is connected.
+IMU.initIMU()       #Initialise the accelerometer, gyroscope and compass
 
 	
-#initialise the accelerometer
-writeACC(CTRL_REG1_XM, 0b01100111) #z,y,x axis enabled, continuos update,  100Hz data rate
-writeACC(CTRL_REG2_XM, 0b00011000) #+/- 8G full scale
-
 
 
 while True:
 	
 	
 	#Read the accelerometer,gyroscope and magnetometer values
-	ACCx = readACCx()
-	ACCy = readACCy()
-	ACCz = readACCz()
+	ACCx = IMU.readACCx()
+	ACCy = IMU.readACCy()
+	ACCz = IMU.readACCz()
 
 	print("##### X = %f G  #####" % ((ACCx * 0.244)/1000)),
 	print(" Y =   %fG  #####" % ((ACCy * 0.244)/1000)),
