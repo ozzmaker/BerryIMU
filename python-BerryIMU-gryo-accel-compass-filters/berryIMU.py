3f509b1cac84a603a6daca6e04f37ce13a69f5d5
mwilliams03 <mark.mwilliams@gmail.com>
2019-08-04 21:47:58 +1000

    Update berryIMU.py

--- python-BerryIMU-gryo-accel-compass-filters/berryIMU.py
+++ python-BerryIMU-gryo-accel-compass-filters/berryIMU.py
@@ -175,7 +175,7 @@ a = datetime.datetime.now()
 
 
 
-#Setup the tables for the mdeian filter. Fill them all with '1' soe we dont get devide by zero error 
+#Setup the tables for the mdeian filter. Fill them all with '1' so we dont get devide by zero error 
 acc_medianTable1X = [1] * ACC_MEDIANTABLESIZE
 acc_medianTable1Y = [1] * ACC_MEDIANTABLESIZE
 acc_medianTable1Z = [1] * ACC_MEDIANTABLESIZE
