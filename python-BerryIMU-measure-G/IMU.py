9c093b92b75b4c81d43e4e14e2dff4c09727e3f0
mwilliams03 <mark.mwilliams@gmail.com>
2021-03-02 16:42:18 +1100

    Removed LPF from accel

--- python-BerryIMU-measure-G/IMU.py
+++ python-BerryIMU-measure-G/IMU.py
@@ -277,7 +277,6 @@ def initIMU():
     elif(BerryIMUversion == 3):       #For BerryIMUv3
         #initialise the accelerometer
         writeByte(LSM6DSL_ADDRESS,LSM6DSL_CTRL1_XL,0b10011111)           #ODR 3.33 kHz, +/- 8g , BW = 400hz
-        writeByte(LSM6DSL_ADDRESS,LSM6DSL_CTRL8_XL,0b11001000)           #Low pass filter enabled, BW9, composite filter
         writeByte(LSM6DSL_ADDRESS,LSM6DSL_CTRL3_C,0b01000100)            #Enable Block Data update, increment during multi byte read
 
         #initialise the gyroscope
